a1d5398cbd4f9b21e218eb8ee2406d36ea1e8cde
Hal Vong <halvong@yahoo.com>
2020-05-09 16:36:20 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+Springit by Dan Vega, Packt video
+
+5/9/2020, Sat
+
+Profiles
+
+#url
+https://subscription.packtpub.com/video/programming/9781839217562/p4/video4_4/profiles
+
+
+
+#createclob is not implemented
+spring.jpa.properties.hibernate.jdbc.lob.non_contextual_creation=true in your "application.yml"
+    or
+hibernate.jdbc.lob.non_contextual_creation=true in your "persistence.xml"
+
+## Contributing
+Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
+## Authors
+* **Dan Vega** - [TheRealDanVega](http://www.therealdanvega.com)
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
---
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -0,0 +1,43 @@
+package com.vega.springit;
+
+import com.vega.springit.config.SpringitProperties;
+import org.ocpsoft.prettytime.PrettyTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.SpringProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+@EnableConfigurationProperties(SpringitProperties.class)
+public class SpringitApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
+
+	@Autowired
+	private SpringitProperties springitProperties;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringitApplication.class, args);
+	}
+
+	@Bean
+	//@Profile("dev")
+	CommandLineRunner runner() {
+		return args -> {
+			System.out.println("--- Welcome Hal! ---" + springitProperties.getWelcomeMsg());
+			//log.info("CommandLineRunner");
+		};
+	}
+
+}
